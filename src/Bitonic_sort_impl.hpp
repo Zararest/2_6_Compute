3157f97149934e69dba36957e0452973336e4589
UwU <iishumakov@gmail.com>
2022-03-31 19:46:16 +0300

    Working CPU sort

--- src/Bitonic_sort_impl.hpp
+++ src/Bitonic_sort_impl.hpp
@@ -1,4 +1,4 @@
-#include "headers/Bitonic_sort.h"
+#include "headers/Bitonic_sort.hpp"
 #include <iostream>
 #include <limits>
 #include <ctime>
@@ -26,7 +26,7 @@ template <typename T>
 cl::Context BitonicSort<T>::get_GPU_context(cl_platform_id cur_platform){
 
     cl_context_properties properties[] = {
-        CL_CONTEXT_PLATFORM, reinterpret_cast<cl_context_properties>(PId),
+        CL_CONTEXT_PLATFORM, reinterpret_cast<cl_context_properties>(cur_platform),
         0
     };
 
@@ -35,34 +35,36 @@ cl::Context BitonicSort<T>::get_GPU_context(cl_platform_id cur_platform){
 
 template <typename T>
 void BitonicSort<T>::split(cl::vector<T>& arr, int from, int to, bool increasing){
+    
+    int middle = (to - from + 1) / 2;
 
-    for (int i = 0; i < (from - to) / 2; i++){
+    for (int i = 0; i < middle; i++){
+        
+        if (increasing && arr[from + i] > arr[from + middle + i]){
 
-        if (increasing && arr[from + i] > arr[to - i]){
-
-            std::swap(arr[from + i], arr[to - i]);
+            std::swap(arr[from + i], arr[from + middle + i]);
         }
 
-        if (!increasing && arr[from + i] < arr[to - i]){
+        if (!increasing && arr[from + i] < arr[from + middle + i]){
 
-            std::swap(arr[from + i], arr[to - i]);
+            std::swap(arr[from + i], arr[from + middle + i]);
         }
     }
 }
 
 template <typename T>
 void BitonicSort<T>::make_mono(cl::vector<T>& arr, int from, int to, bool increasing){
-
-    int bitonic_seq_size = to - from, cur_pos;
+    
+    int bitonic_seq_size = to - from + 1, cur_pos;
     assert(bitonic_seq_size > 0);
 
     while (bitonic_seq_size >= 2){
-
+        
         cur_pos = from;
-
-        for (int i = 0; i < (from - to) / bitonic_seq_size; i++){
-
-            split(arr, cur_pos, cur_pos + bitonic_seq_size, increasing);
+         
+        for (int i = 0; i < (to - from + 1) / bitonic_seq_size; i++){
+            
+            split(arr, cur_pos, cur_pos + bitonic_seq_size - 1, increasing);
             cur_pos += bitonic_seq_size;
         }
 
@@ -71,7 +73,7 @@ void BitonicSort<T>::make_mono(cl::vector<T>& arr, int from, int to, bool increa
 }
 
 template <typename T>
-void BitonicSort<T>::sort_arr(cl::vector<T>& arr){
+void BitonicSort<T>::sort_arr(cl::vector<T>& arr, int size_degree_of_two){
 
     int input_size = std::pow(2, size_degree_of_two);
 
@@ -84,37 +86,47 @@ void BitonicSort<T>::sort_arr(cl::vector<T>& arr){
     bool increasing;
     int cur_pos, bitonic_size;
 
-    for (int chunck_size = 2; chunck_size <= input_size; chunck_size *= 2){
+    for (int chunck_size = 2; chunck_size <= input_size / 2; chunck_size *= 2){
 
         cur_pos = 0;
 
         for (int i = 0; i < input_size / (chunck_size * 2); i++){
 
-            make_mono(arr, cur_pos, cur_pos + chunck_size, true);
+            make_mono(arr, cur_pos, cur_pos + chunck_size - 1, true);
             cur_pos += chunck_size;
-            make_mono(arr, cur_pos, cur_pos + chunck_size, false);
+            make_mono(arr, cur_pos, cur_pos + chunck_size - 1, false);
             cur_pos += chunck_size;
         }
     }
+
+    make_mono(arr, 0, input_size, true);
 }
 
 template <typename T>
 bool BitonicSort<T>::check_sorted_arr(){
+    
+    T prev;
 
-    auto prev_it = sorted_arr.begin();
+    if (!sorted_arr.empty()){
+
+        prev = sorted_arr[0];
+    }
+
     unsigned long cur_check_sum = 0;
-
+    
     for (auto it : sorted_arr){
 
-        if (it < prev_it){
+        if (it < prev){
 
             std::cerr << "Wrong orderind:" 
-            << prev_it* << " > " << it* << std::endl;
+            << prev << " > " << it << std::endl;
 
             return false;
         }
 
-        cur_check_sum += std::hash<T>(it*);
+        prev = it;
+        
+        cur_check_sum += std::hash<T>{}(it);
     }
 
     if (cur_check_sum != check_sum){
@@ -130,9 +142,11 @@ bool BitonicSort<T>::check_sorted_arr(){
 
 
 template <typename T>
-BitonicSort<T>::BitonicSort(): platform_(get_GPU_platform()), 
+BitonicSort<T>::BitonicSort(std::istream& input, std::ostream& output): platform_(get_GPU_platform()), 
                             context_(get_GPU_context(platform_())),
-                            queue_(context_)
+                            queue_(context_),
+                            input_{input},
+                            output_{output}
 {
     cl::string platform_name = platform_.getInfo<CL_PLATFORM_NAME>();
     cl::string platform_profile = platform_.getInfo<CL_PLATFORM_PROFILE>();
@@ -140,13 +154,6 @@ BitonicSort<T>::BitonicSort(): platform_(get_GPU_platform()),
     std::cout << "Platform:\n"
     << "-name: " << platform_name << '\n'
     << "-priofile" << platform_profile << std::endl;           
-}
-
-template <typename T>
-void BitonicSort<T>::set_streams(std::istream& input, std::ostream& output){
-
-    input_ = input;
-    output_ = output;
 }
 
 template <typename T>
@@ -158,7 +165,7 @@ void BitonicSort<T>::read_array(int num_of_elems){
     for (int i = 0; i < num_of_elems; i++){
 
         input_ >> tmp_buf[i];
-        tmp_check_sum += std::hash<T>(tmp_buf[i]);
+        tmp_check_sum += std::hash<T>{}(tmp_buf[i]);
     }
 
     int degree_of_two = 1, new_size = 2;
@@ -172,7 +179,7 @@ void BitonicSort<T>::read_array(int num_of_elems){
     for (int i = 0; i < new_size - num_of_elems; i++){
 
         tmp_buf.push_back(std::numeric_limits<T>::max());
-        tmp_check_sum += std::hash<T>(std::numeric_limits<T>::max());
+        tmp_check_sum += std::hash<T>{}(std::numeric_limits<T>::max());
     }
 
     std::swap(tmp_buf, input_arr);
@@ -184,9 +191,14 @@ void BitonicSort<T>::read_array(int num_of_elems){
 template <typename T>
 void BitonicSort<T>::print_array(){
 
-    for (int i = 0; i < num_of_elems_; i++){
+    int i = 0;
 
-        output_ << sorted_arr[i] << std::endl;
+    for (auto it : sorted_arr){
+
+        if (i >= num_of_elems_) return;
+        i++;
+
+        output_ << it << std::endl;
     }
 }
 
@@ -194,13 +206,13 @@ template <typename T>
 double BitonicSort<T>::CPU_time(){
 
     cl::vector<T> tmp_buf;
-    std::copy(input_arr.begin(), input_arr.end(), std::back_insert_iterator(tmp_buf));
+    std::copy(input_arr.begin(), input_arr.end(), std::back_insert_iterator<cl::vector<T>>(tmp_buf));
 
     clock_t start = clock();
-    sort_arr(tmp_buf);
+    sort_arr(tmp_buf, size_degree_of_two);
     clock_t end = clock();
 
-    std::swap(tmp_buf, sort_arr);
+    std::swap(tmp_buf, sorted_arr);
 
-    return reinterpret_cast<double>(end - start) / CLOCKS_PER_SEC;
+    return static_cast<double>(end - start) / CLOCKS_PER_SEC;
 }
--- src/headers/Bitonic_sort.hpp
+++ src/headers/Bitonic_sort.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+#ifndef CL_HPP_TARGET_OPENCL_VERSION
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#endif
+
+#define CL_HPP_CL_1_2_DEFAULT_BUILD
+#define CL_HPP_ENABLE_EXCEPTIONS
+
 #include "../../lib/opencl.hpp"
 #include <iostream>
 
@@ -22,20 +30,21 @@ class BitonicSort{
     static cl::Platform get_GPU_platform();
     static cl::Context  get_GPU_context(cl_platform_id cur_platform);
 
-    static void sort_arr(cl::vector<T>& arr);
+    static void sort_arr(cl::vector<T>& arr, int size_degree_of_two);
     static void make_mono(cl::vector<T>& arr, int from, int to, bool increasing);
     static void split(cl::vector<T>& arr, int from, int to, bool increasing);
-    
-    bool check_sorted_arr();
 
 public:
 
-    BitonicSort();
+    BitonicSort(std::istream& input, std::ostream& output = std::cout);
 
-    void set_streams(std::istream& input, std::ostream& output = std::cout);
     void read_array(int num_of_elems);
     void print_array();
+    bool check_sorted_arr();
 
     double CPU_time();
     std::pair<double, double> GPU_time();    
 };
+
+
+#include "../Bitonic_sort_impl.hpp"
